a37ad7266ceea1fde15fb9681ccc88febc6705b6
Vogrič <53317134+Vogric@users.noreply.github.com>
2020-07-24 18:35:19 -0300

    docs

---
+++ docs/precache-manifest.4bf1b5c4a76a960ee8411fffc5b060ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98fecc50b8a9a09245462259e3641d14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b70892b52d156a3931c",
+    "url": "/static/css/main.38fada15.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b70892b52d156a3931c",
+    "url": "/static/js/main.98eaa29c.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  },
+  {
+    "revision": "e99bf62502553639c780df9a40d3f919",
+    "url": "/static/media/10.e99bf625.gif"
+  }
+]);
